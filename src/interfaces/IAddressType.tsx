fe3d224abb0a257e22996ee46cf053a82ff5e2a1
Nazim <nahmedov347@gmail.com>
2024-08-06 22:42:11 +0200

    update on ALB connection

--- src/interfaces/IAddressType.tsx
+++ src/interfaces/IAddressType.tsx
@@ -1,7 +1,7 @@
 export interface AddressType {
+    id: number | null; // Assuming null or number based on your data
     city: string;
     country: string;
     postalCode: string;
     street: string;
-    id: number | null; // Assuming null or number based on your data
 }
